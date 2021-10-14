67d664331502f06f11519e89ab42ff8d7637acb1
Francis Dias <francisdiasbr@gmail.com>
2021-10-13 22:00:28 -0300

    Cria as rotas para os módulos pilots, races e teams

--- server.js
+++ server.js
@@ -2,22 +2,16 @@ const cors = require('cors')
 const express = require('express')
 const server = express()
 
+const circuits = require('./circuits.json')
 const functions = require('./functions')
-
-const circuits = []
+const pilots = require('./pilots.json')
+const races = require('./races.json')
+const teams = require('./teams.json')
 
 server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({ extended: true}))
 
-server.get('/', (req, res) => {
-    console.log('rota raiz')
-    res.send('rota raiz no client!')
-})
-server.get('/rota2/:id', (req, res) => {
-    console.log(`chamou no server ${req.params.id}`)
-    res.status(200).json(req.params.id)
-})
 server.get('/standings/:pos', (req, res) => {
     const pos = req.params.pos
     const points = functions.calcPoints(pos)
@@ -31,11 +25,30 @@ server.post('/circuits', (req, res) => {
 server.get('/circuits', (req, res) => {
     res.status(200).json(circuits)
 })
-server.get('/:id', (req, res) => {
-    console.log(`chamou no server ${req.params.id}`)
-    const resultado = req.params.id / 5
-    res.status(200).json(resultado)
+server.post('/pilots', (req, res) => {
+    console.log(req.body)
+    pilots.push(req.body)
+    res.status(200).json({sucess:true})
+})
+server.get('/pilots', (req, res) => {
+    res.status(200).json(pilots)
+})
+server.post('/races', (req, res) => {
+    console.log(req.body)
+    races.push(req.body)
+    res.status(200).json({sucess:true})
+})
+server.get('/races', (req, res) => {
+    res.status(200).json(races)
+})
+server.post('/teams', (req, res) => {
+    console.log(req.body)
+    teams.push(req.body)
+    res.status(200).json({sucess:true})
+})
+server.get('/teams', (req, res) => {
+    res.status(200).json(teams)
 })
 server.listen(3001, () => {
-    console.log('meu primeiro servidor subiu')
+    console.log('F1 server subiu')
 })
